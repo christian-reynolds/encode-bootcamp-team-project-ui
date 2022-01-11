278bfcbe4b786b65e81bd0d4df30ed4a548369de
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-11 15:56:40 -0500

    Added web3-react and ethers

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
+import { Web3ReactProvider } from '@web3-react/core';
+import { providers } from 'ethers';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const getLibrary = (provider: any) => new providers.Web3Provider(provider);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Web3ReactProvider>
   );
 }
 
