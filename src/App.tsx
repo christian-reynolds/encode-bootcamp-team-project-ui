fb37700061520e7f8d93f8da8ea91a78ee51cbe3
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-18 07:27:11 -0500

    Added react-router-dom and setup the main routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Web3ReactProvider } from '@web3-react/core';
 import { providers } from 'ethers';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main';
 
@@ -9,10 +10,21 @@ function App() {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <div className="App">
-          <header className="App-header">
-            <Main />
-          </header>
-        </div>
+        <header className="App-header">
+          <BrowserRouter>
+            <Routes>
+              <Route 
+                path="/"
+                element={<Main />}
+              />
+              <Route 
+                path="/account"
+              />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </BrowserRouter>
+        </header>
+      </div>
     </Web3ReactProvider>
   );
 }
