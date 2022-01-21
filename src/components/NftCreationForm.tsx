7e1daaeafef0b7cf33bb66e3f3d39efd1bbe1525
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-21 06:49:49 -0500

    Got the image saving to state

--- src/components/NftCreationForm.tsx
+++ src/components/NftCreationForm.tsx
@@ -13,7 +13,20 @@ function NftCreationForm() {
     const navigate = useNavigate();
     const tokenId = params.tokenId;
     const { account } = useWeb3React<providers.Web3Provider>();
-    const [complete, setComplete] = useState(false);
+    const [selectedFile, setSelectedFile] = useState<File>();
+
+    // On file select (from the pop up)
+    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const test = event.target.files && event.target.files[0];
+
+        // Update the state
+        setSelectedFile(test!);
+    };
+
+    const onClick = () => {
+        console.log(selectedFile);
+    };
+    
 
     // TODO: There is a bug with this logic.
     // If there is no account connected then it allows you to stay on the page when you shouldn't be. Not sure how to handle this right now
@@ -34,7 +47,7 @@ function NftCreationForm() {
     return (
         <div className="flex flex-wrap justify-center items-center w-full">
             <div className="w-full bg-gray-200 rounded shadow-2xl p-8 m-4"> 
-                {complete &&
+                {selectedFile &&
                     <p className="block w-full text-center text-red-400 text-base font-bold mb-6">
                         Your nft has been created!
                     </p>
@@ -44,9 +57,9 @@ function NftCreationForm() {
                 </p>
                 <div className="flex flex-col mb-4">
                     <label className="mb-2 font-bold text-lg text-left text-gray-600">Select Image</label>
-                    <input className="border py-2 px-3 text-sm text-black" placeholder="Select Image" type="image" />
+                    <input className="border py-2 px-3 text-sm text-black" placeholder="Select Image" type="file" onChange={onFileChange} />
                 </div>
-                <Button label="Create NFT" className="block bg-gray-400 hover:bg-gray-600 text-white uppercase text-sm mx-auto p-4 rounded" />
+                <Button label="Create NFT" className="block bg-gray-400 hover:bg-gray-600 text-white uppercase text-sm mx-auto p-4 rounded" onClick={onClick} />
             </div>
         </div>
     );
