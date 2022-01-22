afb75d59eb5eddeeb8db93a1cebd419ff507ed4b
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-22 13:16:39 -0500

    Checking in a hardcoded proof

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -55,11 +55,14 @@ export const callContractView = async (provider: providers.Web3Provider, contrac
   return String(data);
 };
 
-export const claimNft = async (provider: providers.Web3Provider, contractAddress: string, proof: string) => {
+export const claimNft = async (provider: providers.Web3Provider, contractAddress: string, proof: string[]) => {
   // TODO: Figure out how to get the ERC721 contract address and replace the hardcoded address
-  const contract = new Contract("0xd341367d86bEE78763E91627910C8F1FBA0fdC4d", BASE_ERC721.abi, provider.getSigner());
+  const contract = new Contract("0xd88f791bf1e939727d7e4f9027c1cfbe0029f5b0", BASE_ERC721.abi, provider.getSigner());
   // TODO: Fix the error that is happening with passing the proof to the function
-  const tx = await contract.claim(utils.toUtf8Bytes(proof));
+
+  const test = ["0x58248f99b36b9446b2bd4020733ba808a3d9c153921f6465355d66211abb6d2d","0x4cf12ef96bfa6e34bd3489454bbf83e126c31d9ef6aa336129f61468b5c33d5a","0xf2a9ab8585283349d100869437e350504785f4bdd9d5420846e6c4c33c5dbe61","0x39fafd32ee8531d5959eee929a7466ff842e69f5e3456de6d3881a99eceeb0b5","0x26d11ba94fce02cb28e0eed5854aff9032f9b552b49d4671c1e5f6ccd5d87f0c","0xa9dbaa6573d178b663971ca135e9653b55c90e86909fd52535af7a54e5427ae2"];
+
+  const tx = await contract.claim(test);
   await tx.wait();
   
   console.log("claimNft: ", tx.hash);
