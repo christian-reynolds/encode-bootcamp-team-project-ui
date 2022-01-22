dd1af00ca99e6a399c84874c7072b4696b176fa6
d1m3 <helloyesplease33@gmail.com>
2022-01-22 14:35:57 -0600

    added example contract from our UI

--- src/testBalanceTracking.js
+++ src/testBalanceTracking.js
@@ -1,12 +1,12 @@
 const Web3 = require('web3')
 //change mainnet to appropriate testnet: ropsten, rinkeby, goerli, kovan
-const infura_RPC_URL = 'shared_in_discord'
+const infura_RPC_URL = "shared_in_discord"
 const web3 = new Web3(infura_RPC_URL)
+const BASE_ERC20 = require('./BaseErc20.json')
 
-//OMG erc-20 contract abi and address as an example, should be the deployed erc-20 contract's abi & address
-const abi = [{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
-const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'
-const example_block_number = 14046200 // as an example put in a recent block # from ethereum mainnet (last 50 blocks or so)
+const abi = BASE_ERC20.abi
+const address = '0x3f990D28D725D439bd097E8f142C23dd0DCDCb92' //example ERC-20 deployed from our contract
+const example_block_number = 1003700 // an example block # from the rinkeby network just before the contract's deployment
 
 const contract = new web3.eth.Contract(abi,address)
 //balances dict
@@ -15,9 +15,9 @@ balances = {}
 async function getBalances() { 
     
     const events = await contract.getPastEvents( 
-    'Transfer',{
+      'Transfer',{
         fromBlock:  example_block_number, // fromBlock should be the block when the erc-20 contract was deployed
-        toBLock: 'latest'}) //note: this fromBlock, toBlock filter is likely unecessary for our purposes (since we want all transfers)
+        toBLock: 'latest'}) // toBLock: 'latest'}) 
 
     //add all addresses to the balances dict
     events.forEach(event=> balances[event.returnValues.to]=0)
